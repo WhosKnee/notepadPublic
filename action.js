4383f50ce9f814b2c6483e2a2b4abb3ecb3c4c67
WhosKnee <husnifareed9@gmail.com>
2019-03-21 07:56:02 -0400

    Made the new note section have a division and made it inline so that stored notes can be stored on the right side

--- action.js
+++ action.js
@@ -2,12 +2,23 @@
 var title = " ";
 var text = " ";
 
+// create template for card that holds note
+var cardTemplate = 
+"<div class='card'>\n" +
+"   <div class='card-body'>\n" +
+"       <h5 class='card-title'>Test</h5>\n" +
+"       <p class='card-text'>TEST TEXT</p>\n" +
+"   </div>\n" +
+"</div>\n";
+
 function newNote () {
-    document.getElementsByClassName('newNote')[0].style.display = "block";
+    document.getElementsByClassName('newNote')[0].style.visibility = "visible";
 }
 
 function save () {
     title = document.getElementById('title').value;
     text = document.getElementById('textFill').value;
-    $("<div>" + title +" ------- " + text + "</div>").appendTo("body");
+    $(cardTemplate).appendTo("body");
 }
+
+
